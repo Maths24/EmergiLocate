b6c366dfe236fd8ced7f3f6604087497c94725cd
Matthias Meierlohr <matthi24.m@web.de>
2024-03-21 09:10:26 +0100

    Initial Commit

---
+++ EmergiLocate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmergiLocate
+//
+//  Created by Matthias Meierlohr on 21.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmergiLocate/EmergiLocateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmergiLocateApp.swift
+//  EmergiLocate
+//
+//  Created by Matthias Meierlohr on 21.03.24.
+//
+
+import SwiftUI
+
+@main
+struct EmergiLocateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
